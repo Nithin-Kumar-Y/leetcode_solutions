d76f765052c8025b7b31954ed67ae3b8f318dd71
Nithin Kumar Y <nithinkumar.y.2050@gmail.com>
2026-09-05 08:32:11 +0530

    Time: 274 ms (87.1%), Space: 22.3 MB (48.39%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        maxi = [0]
+        mini = [float('inf')]
+        for i in nums:
+            if i> maxi[-1]:
+                maxi.append(i)
+            else:
+                maxi.append(maxi[-1])
+        for i in reversed(nums):
+            if i< mini[-1]:
+                mini.append(i)
+            else:
+                mini.append(mini[-1])
+        right = mini[::-1]
+        for i in range(len(nums)):
+            if maxi[i+1]-right[i]<=k:
+                return i
+        return -1

521b2e928cdd1507164960df6064ec8f6000051e
Nithin Kumar Y <nithinkumar.y.2050@gmail.com>
2026-09-23 23:17:02 +0530

    Time: 4 ms (12.87%), Space: 12.4 MB (38.32%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -4,29 +4,14 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
-        if obstacleGrid[0][0]==1 or obstacleGrid[-1][-1]==1:
-            return 0
-        m = len(obstacleGrid[0])  # columns
-        n = len(obstacleGrid)  # rows
-        dp = [[0]*(m+1) for _ in range(n+1)]
-        
-        dp[1][1]=1
-        # base case
-        for i in range(2, m+1):
-            if obstacleGrid[0][i-1] == 1:
-                dp[1][i] = 0
-            else:
-                dp[1][i]=dp[1][i-1]
-        for i in range(2, n+1):
-            if obstacleGrid[i-1][0] == 1:
-                dp[i][1] = 0
-            else:
-                dp[i][1]= dp[i-1][1]
-        
-        # transition state
-        for i in range(2, n+1):
-            for j in range(2, m+1):
-                if obstacleGrid[i-1][j-1] != 1:
-                    dp[i][j]= dp[i-1][j]+dp[i][j-1]
-        print(dp)
-        return dp[n][m]
+        n = len(obstacleGrid[0])
+        dp= [0]*n
+        dp[0]=1
+
+        for i in range(len(obstacleGrid)):
+            for j in range(n):
+                if obstacleGrid[i][j]==1:
+                    dp[j]=0
+                elif j>0:  ## how this step??
+                    dp[j]+=dp[j-1]
+        return dp[n-1]
